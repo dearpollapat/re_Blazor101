362667924218cfecddcf45515ded1e66daf10f8d
dearpollapat <pollapat.dear@gmail.com>
2024-10-28 14:42:29 +0700

    two way data binding and send parameter component

---
+++ EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -0,0 +1,13 @@
+﻿using EmploymentManagement.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DisplayEmployeeBase : ComponentBase
+    {
+        [Parameter]
+        public Employee Employee { get; set; }
+        [Parameter]
+        public bool ShowFooter { get; set; }
+    }
+}
