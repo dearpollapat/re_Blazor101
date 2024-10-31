01af5ea5d2afee25d909ba235e387a1920cdfd14
dearpollapat <pollapat.dear@gmail.com>
2024-10-31 20:38:32 +0700

    Building Blazor form [Edit form]

---
+++ EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement.Web.Services;
+using EmploymentManagement.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EditEmployeeBase : ComponentBase
+    {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+        public Employee Employee { get; set; } = new Employee();
+        [Parameter]
+        public string Id { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}
